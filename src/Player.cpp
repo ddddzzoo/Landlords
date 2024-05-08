5dfd882ca26a2385a595e287dc07c8aed3e7039c
Yee <873720581@qq.com>
2024-05-08 14:34:48 +0800

    初步添加玩家基类属性

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,4 +2,36 @@
 
 Player::Player(QObject *parent) : QObject(parent) {}
 
+Player::Player(QString &name, QObject *parent) : QObject(parent) {
+  _name = name;
+}
+
 Player::~Player() {}
+
+void Player::setName(QString &name) { _name = name; }
+
+QString Player::getName() const { return _name; }
+
+void Player::setRole(Role role) { _role = role; }
+
+Player::Role Player::getRole() const { return _role; }
+
+void Player::setSex(Sex sex) { _sex = sex; }
+
+Player::Sex Player::getSex() const { return _sex; }
+
+void Player::setDirection(Direction direction) { _direction = direction; }
+
+Player::Direction Player::getDirection() const { return _direction; }
+
+void Player::setType(Type type) { _type = type; }
+
+Player::Type Player::getType() const { return _type; }
+
+void Player::setScore(int score) { _score = score; }
+
+int Player::getScore() const { return _score; }
+
+void Player::setWin(bool flag) { _isWin = flag; }
+
+bool Player::isWin() const { return _isWin; }
--- src/Player.h
+++ src/Player.h
@@ -6,6 +6,48 @@ class Player : public QObject {
   Q_OBJECT
 
  public:
+  enum Role { Lord, Farmer };         // 角色
+  enum Sex { Man, Woman };            // 性别
+  enum Direction { Left, Right };     // 头像的显示方向
+  enum Type { Robot, User, UnKnow };  // 玩家类型
   Player(QObject *parent);
+  explicit Player(QString &name, QObject *parent = nullptr);
   ~Player();
+
+  // 名字
+  void setName(QString &name);
+  QString getName() const;
+
+  // 角色
+  void setRole(Role role);
+  Role getRole() const;
+
+  // 性别
+  void setSex(Sex sex);
+  Sex getSex() const;
+
+  // 头像显示方位
+  void setDirection(Direction direction);
+  Direction getDirection() const;
+
+  // 玩家类型
+  void setType(Type type);
+  Type getType() const;
+
+  // 玩家分数
+  void setScore(int score);
+  int getScore() const;
+
+  // 游戏输赢
+  void setWin(bool flag);
+  bool isWin() const;
+
+ private:
+  QString _name{};
+  Role _role{};
+  Sex _sex{};
+  Direction _direction{};
+  Type _type{};
+  int _score{0};
+  bool _isWin{false};
 };
